bf5d6242dd2dd1b01c529efa0d219448e61fdfa0
Thomas Tardi-Zuch <thomas.tardi-zuch@hatchinvest.nz>
2023-04-27 13:56:30 +1200

    Update to search and flash

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,15 +1,8 @@
 class SearchController < ApplicationController
   def index
-    @search_results = PgSearch.multisearch(search_params).map do |result|
-      case result.searchable_type
-      when "User"
-        User.find(result.searchable_id)
-      when "Tool"
-        Tool.find(result.searchable_id)
-      when "Category"
-        Category.find(result.searchable_id)
-      end
-    end
+    @search_results = PgSearch
+                        .multisearch(search_params)
+                        .map { |result| result.searchable_type.constantize.find(result.searchable_id) }
   end
 
   private
