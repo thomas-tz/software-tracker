855a57768fb101c923f0b1cc19de27125eb5a7d5
Thomas Tardi-Zuch <thomas.tardi-zuch@hatchinvest.nz>
2023-05-01 14:17:39 +1200

    Updated model tests with AJ's feedback

--- spec/models/tool_spec.rb
+++ spec/models/tool_spec.rb
@@ -5,44 +5,32 @@ RSpec.describe Tool, type: :model do
   let!(:tool_with_category) { create(:tool, :with_category) }
 
   describe '.uncategorized' do
-    let(:uncategorized) { Tool.uncategorized }
+    let(:uncategorized) { described_class.uncategorized }
     it 'is all the tools without a category' do
-      aggregate_failures do
-        expect(uncategorized).to_not include(tool_with_category)
-        expect(uncategorized).to contain_exactly(tool)
-      end
+      expect(uncategorized).to contain_exactly(tool)
     end
   end
 
   describe '.not_in_category' do
     let(:category) { tool_with_category.category }
-    let(:not_in_category) { Tool.not_in_category(category) }
+    let(:not_in_category) { described_class.not_in_category(category) }
     it 'is all the tool records not associated with a particular category' do
-      aggregate_failures do
-        expect(not_in_category).to_not include(tool_with_category)
-        expect(not_in_category).to contain_exactly(tool)
-      end
+      expect(not_in_category).to contain_exactly(tool)
     end
   end
 
   describe '.not_subscribed' do
     let(:user) { tool.users.first }
-    let(:not_subscribed) { Tool.not_subscribed(user) }
+    let(:not_subscribed) { described_class.not_subscribed(user) }
     it 'is all the tool records not associated with a particular user' do
-      aggregate_failures do
-        expect(not_subscribed).to_not include(tool)
-        expect(not_subscribed).to contain_exactly(tool_with_category)
-      end
+      expect(not_subscribed).to contain_exactly(tool_with_category)
     end
   end
 
   describe '.user_counts' do
-    let(:user_count) { Tool.user_counts }
+    let(:user_count) { described_class.user_counts }
     it 'counts the number of users that are associated with each tool record' do
-      aggregate_failures do
-        expect(user_count).to_not have_key(tool_with_category.id)
-        expect(user_count).to eq({ tool.id => tool.users.count })
-      end
+      expect(user_count).to eq({ tool.id => tool.users.count })
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,27 +6,21 @@ RSpec.describe User do
 
   describe '#name' do
     it 'is the user\'s first and last name' do
-      expect(subject.name).to eq "#{subject.first_name} #{subject.last_name}"
+      expect(user.name).to eq "#{user.first_name} #{user.last_name}"
     end
   end
 
   describe '.not_subscribed' do
     it 'is all the user records not associated with a particular tool' do
-      aggregate_failures do
-        expect(User.not_subscribed(tool)).to contain_exactly(user)
-        expect(User.not_subscribed(tool)).to_not include(tool.users.first)
-      end
+      expect(described_class.not_subscribed(tool)).to contain_exactly(user)
     end
   end
 
   describe '.tool_counts' do
-    let(:tool_count) { User.tool_counts }
+    let(:tool_count) { described_class.tool_counts }
     let(:tool_user) { tool.users.first }
     it 'counts the number of tools that are associated with each user record' do
-      aggregate_failures do
-        expect(tool_count).to_not have_key(user.id)
-        expect(tool_count).to eq({ tool_user.id => tool_user.tools.count})
-      end
+      expect(tool_count).to eq({ tool_user.id => tool_user.tools.count})
     end
   end
 end
